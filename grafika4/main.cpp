0c634da21c7b4310b0b12ed2dfe7f5a1e001ef2b
azamiszuhri <azamiszuhri@gmail.com>
2016-02-11 22:37:28 +0700

    gazanditampansekali

--- grafika4/main.cpp
+++ grafika4/main.cpp
@@ -3,268 +3,44 @@
 using namespace std;
 
 FrameBuffer fb;
-Polygon* printA(){
+
+Polygon* printpeluru(){
     Polygon* pol = new Polygon();
-    pol->addPoint(Point(0, 600));
-    pol->addPoint(Point(150, 600));
-    pol->addPoint(Point(300, 370));
-    pol->addPoint(Point(400, 370));
-    pol->addPoint(Point(400, 270));
-    pol->addPoint(Point(330, 270));
-    pol->addPoint(Point(400, 140));
-    pol->addPoint(Point(470, 270));
-    pol->addPoint(Point(400, 270));
-    pol->addPoint(Point(400, 370));
-    pol->addPoint(Point(500, 370));
-    pol->addPoint(Point(650, 600));
-    pol->addPoint(Point(800, 600));
-    pol->addPoint(Point(400, 0));
-    return pol;
-}
-Polygon* printZ(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(0, 0));
-    pol->addPoint(Point(800, 0));
-    pol->addPoint(Point(800, 100));
-    pol->addPoint(Point(200, 500));
-    pol->addPoint(Point(800, 500));
-    pol->addPoint(Point(800, 600));
-    pol->addPoint(Point(0, 600));
-    pol->addPoint(Point(0, 500));
-     pol->addPoint(Point(600, 100));
-    pol->addPoint(Point(0, 100));
-    return pol;
-}
-Polygon* printN(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(0, 0));
-    pol->addPoint(Point(100, 0));
-    pol->addPoint(Point(700, 450));
-    pol->addPoint(Point(700, 0));
-    pol->addPoint(Point(800, 0));
-    pol->addPoint(Point(800, 600));
-    pol->addPoint(Point(700, 600));
-    pol->addPoint(Point(100, 150));
-    pol->addPoint(Point(100, 600));
-    pol->addPoint(Point(0, 600));
-    return pol;
-}
-Polygon* printD(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(200, 0));
-    pol->addPoint(Point(300, 0));
-    pol->addPoint(Point(300, 500));
-    pol->addPoint(Point(450, 450));
-    pol->addPoint(Point(500, 425));
-    pol->addPoint(Point(500, 175));
-    pol->addPoint(Point(450, 150));
-    pol->addPoint(Point(300, 100));
-     pol->addPoint(Point(300, 0));
-    pol->addPoint(Point(450, 50));
-    pol->addPoint(Point(600, 175));
-    pol->addPoint(Point(600, 425));
-    pol->addPoint(Point(450, 550));
-    pol->addPoint(Point(300, 600));
-    pol->addPoint(Point(200, 600));
-    return pol;
-}
-Polygon* printI(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(350, 0));
-    pol->addPoint(Point(450, 0));
-    pol->addPoint(Point(450, 600));
-    pol->addPoint(Point(350, 600));
-    return pol;
-}
-Polygon* printL(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(200, 0));
-    pol->addPoint(Point(300, 0));
-    pol->addPoint(Point(300, 500));
-    pol->addPoint(Point(600, 500));
-    pol->addPoint(Point(600, 600));
-    pol->addPoint(Point(200, 600));
-    return pol;
-}
-Polygon* printU(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(100, 0));
-    pol->addPoint(Point(200, 0));
-    pol->addPoint(Point(200, 500));
-    pol->addPoint(Point(600, 500));
-    pol->addPoint(Point(600, 0));
-    pol->addPoint(Point(700, 0));
-    pol->addPoint(Point(700, 600));
-    pol->addPoint(Point(100, 600));
-    return pol;
-}
-Polygon* printC(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(100, 50));
-    pol->addPoint(Point(150, 0));
-    pol->addPoint(Point(700, 0));
-    pol->addPoint(Point(700, 100));
-    pol->addPoint(Point(200, 100));
-    pol->addPoint(Point(200, 500));
-    pol->addPoint(Point(700, 500));
-    pol->addPoint(Point(700, 600));
-    pol->addPoint(Point(150, 600));
-    pol->addPoint(Point(100, 550));
+    pol->addPoint(Point(300,600));
+    pol->addPoint(Point(500,600));
+    pol->addPoint(Point(500,200));
+    pol->addPoint(Point(550,200));
+    pol->addPoint(Point(450,0));
+    pol->addPoint(Point(350,0));
+    pol->addPoint(Point(250,200));
+    pol->addPoint(Point(300,200));
     return pol;
 }
 
-Polygon* printQ(){
+Polygon* printsijagur(){
     Polygon* pol = new Polygon();
-    pol->addPoint(Point(0, 50));
-    pol->addPoint(Point(50, 0));
-    pol->addPoint(Point(500, 0));
-    pol->addPoint(Point(500, 100));
-    pol->addPoint(Point(100, 100));
-    pol->addPoint(Point(100, 500));
-    pol->addPoint(Point(500, 500));
-    pol->addPoint(Point(500, 0));
-    pol->addPoint(Point(600, 0));
-    pol->addPoint(Point(600, 400));
-    pol->addPoint(Point(800, 600));
-    pol->addPoint(Point(700, 600));
-    pol->addPoint(Point(600, 520));
-    pol->addPoint(Point(600, 600));
-    pol->addPoint(Point(50, 600));
-    pol->addPoint(Point(0, 550));
+    pol->addPoint(Point(800,600));
+    pol->addPoint(Point(800,500));
+    pol->addPoint(Point(700,500));
+    pol->addPoint(Point(700,300));
+    pol->addPoint(Point(600,300));
+    pol->addPoint(Point(600,0));
+    pol->addPoint(Point(500,0));
+    pol->addPoint(Point(500,200));
+    pol->addPoint(Point(300,200));
+    pol->addPoint(Point(300,0));
+    pol->addPoint(Point(200,0));
+    pol->addPoint(Point(200,500));
+    pol->addPoint(Point(200,300));
+    pol->addPoint(Point(100,300));
+    pol->addPoint(Point(100,500));
+    pol->addPoint(Point(0,500));
+    pol->addPoint(Point(0,600));
     return pol;
 }
-Polygon* printM(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(0, 0));
-    pol->addPoint(Point(100, 0));
-    pol->addPoint(Point(400, 200));
-    pol->addPoint(Point(700, 0));
-    pol->addPoint(Point(800, 0));
-    pol->addPoint(Point(800, 600));
-    pol->addPoint(Point(700, 600));
-    pol->addPoint(Point(700, 100));
-    pol->addPoint(Point(400, 300));
-    pol->addPoint(Point(100, 100));
-    pol->addPoint(Point(100, 600));
-    pol->addPoint(Point(0, 600));
-    return pol;
-}
-Polygon* printR(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(100, 0));
-    pol->addPoint(Point(500, 0));
-    pol->addPoint(Point(700, 120));
-    pol->addPoint(Point(700, 240));
-    pol->addPoint(Point(500, 360));
-    pol->addPoint(Point(700, 480));
-    pol->addPoint(Point(700, 360));
-    pol->addPoint(Point(700, 600));
-    pol->addPoint(Point(500, 600));
-    pol->addPoint(Point(500, 480));
-    pol->addPoint(Point(301, 480));
-    pol->addPoint(Point(301, 240));
-    pol->addPoint(Point(500, 240));
-    pol->addPoint(Point(500, 120));
-    pol->addPoint(Point(301, 120));
-    pol->addPoint(Point(300, 480));
-    pol->addPoint(Point(300, 600));
-    pol->addPoint(Point(100, 600));
-    return pol;
-}
-Polygon* printH(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(100, 0));
-    pol->addPoint(Point(300, 0));
-    pol->addPoint(Point(300, 200));
-    pol->addPoint(Point(500, 200));
-    pol->addPoint(Point(500, 0));
-    pol->addPoint(Point(700, 0));
-    pol->addPoint(Point(700, 600));
-    pol->addPoint(Point(500, 600));
-    pol->addPoint(Point(500, 400));
-    pol->addPoint(Point(300, 400));
-    pol->addPoint(Point(300, 600));
-    pol->addPoint(Point(100, 600));
-    return pol;
-}
-Polygon* printE(){
-    Polygon* pol = new Polygon();
-    pol->addPoint(Point(100, 0));
-    pol->addPoint(Point(700, 0));
-    pol->addPoint(Point(700, 120));
-    pol->addPoint(Point(300, 120));
-    pol->addPoint(Point(300, 240));
-    pol->addPoint(Point(700, 240));
-    pol->addPoint(Point(700, 360));
-    pol->addPoint(Point(300, 360));
-    pol->addPoint(Point(300, 480));
-    pol->addPoint(Point(700, 480));
-    pol->addPoint(Point(700, 600));
-    pol->addPoint(Point(100, 600));
-    return pol;
-}
-Polygon* proto[30];
-Polygon* words[5][10];
-string names[] = {"ZANDI", "ICAL", "AZAM", "LUQMAN", "HERI"};
 
-void init() {
-  proto['A' - 'A'] = printA();
-  proto['Z' - 'A'] = printZ();
-  proto['M' - 'A'] = printM();
-  proto['N' - 'A'] = printN();
-  proto['D' - 'A'] = printD();
-  proto['I' - 'A'] = printI();
-  proto['C' - 'A'] = printC();
-  proto['L' - 'A'] = printL();
-  proto['U' - 'A'] = printU();
-  proto['Q' - 'A'] = printQ();
-  proto['H' - 'A'] = printH();
-  proto['E' - 'A'] = printE();
-  proto['R' - 'A'] = printR();
-  for(int i = 0; i < 5; i++) {
-    for(int j = 0; j < names[i].size(); j++) {
-      words[i][j] = new Polygon(*proto[names[i][j] - 'A']);
-      words[i][j]->resize(Point(300, 400), 0.1);
-      words[i][j]->move(-100+j*80, -300+(i*70));
-    }
-  }
-}
-void print(){
-  for(int i = 0; i < 5; i++) {
-    for(int j = 0; j < names[i].size(); j++) {
-      words[i][j]->print(fb);
-    }
-  }
-}
-void resize_move(float skala){
-  for(int i = 0; i < 5; i++) {
-    for(int j = 0; j < names[i].size(); j++) {
-      words[i][j]->resize(Point(400, 200), skala);
-    }
-  }
-}
-void run(){
-  init();
-  float rasio=1;
-  while(rasio<=3) {
-    print();
-    rasio+=0.1;
-    resize_move(1.05);
-    usleep(50000);
-    fb.clear();
-  }
-  rasio = 0;
-  while(rasio<=3) {
-    print();
-    rasio+=0.1;
-    resize_move(0.95);
-    usleep(50000);
-    fb.clear();
-  }
-}
 int main() {
   system("clear");
-  //printA()->print(fb);
-  run();
+  printpeluru()->print(fb);
   return 0;
 }
