0c607b79d5128717750887d23ab17a5093a56400
icalF <laser.survivor@gmail.com>
2016-02-11 18:25:22 +0700

    rotate polygon

---
+++ grafika4/lib/color.h
@@ -0,0 +1,25 @@
+#ifndef COLOR_H__
+#define COLOR_H__
+
+/**
+ * Class that represents color (defined by red, green, blue, and alpha values)
+ */
+class Color {
+public:
+  Color() : red(0), green(0), blue(0), alpha(0) {}
+  Color(char _red, char _green, char _blue, char _alpha = 0) :
+    red(_red), green(_green), blue(_blue), alpha(_alpha) {}
+  char red, green, blue, alpha;
+
+  bool operator== (const Color &rhs) const {
+    return ((red == rhs.red) and (green == rhs.green) and (blue == rhs.blue) and (alpha == rhs.alpha));
+  }
+
+  bool operator!= (const Color &rhs) const {
+    return (!(*this == rhs));
+  }
+  /* static constants */
+  static const Color RED, GREEN, BLUE, YELLOW, PURPLE, CYAN, BLACK, WHITE, EMPTY;
+};
+
+#endif
