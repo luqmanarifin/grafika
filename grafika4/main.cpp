5c5f1caac1922652bb205e136584d59435bb01a9
azamiszuhri <azamiszuhri@gmail.com>
2016-02-12 22:15:42 +0700

    roda

--- grafika4/main.cpp
+++ grafika4/main.cpp
@@ -39,8 +39,69 @@ Polygon* printsijagur(){
     return pol;
 }
 
+Polygon* printroda(){
+    Polygon* pol = new Polygon();
+    pol->addPoint(Point(325,90));
+    pol->addPoint(Point(475,90));
+    pol->addPoint(Point(475,120));
+    pol->addPoint(Point(535,120));
+    pol->addPoint(Point(535,150));
+    pol->addPoint(Point(565,150));
+    pol->addPoint(Point(565,180));
+    pol->addPoint(Point(595,180));
+    pol->addPoint(Point(595,210));
+    pol->addPoint(Point(625,210));
+    pol->addPoint(Point(625,270));
+    pol->addPoint(Point(655,270));
+    pol->addPoint(Point(655,420));
+    pol->addPoint(Point(625,420));
+    pol->addPoint(Point(625,480));
+    pol->addPoint(Point(595,480));
+    pol->addPoint(Point(595,510));
+    pol->addPoint(Point(565,510));
+    pol->addPoint(Point(565,540));
+    pol->addPoint(Point(535,540));
+    pol->addPoint(Point(535,570));
+    pol->addPoint(Point(475,570));
+    pol->addPoint(Point(475,600));
+    pol->addPoint(Point(325,600));
+    pol->addPoint(Point(325,570));
+    pol->addPoint(Point(265,570));
+    pol->addPoint(Point(265,540));
+    pol->addPoint(Point(235,540));
+    pol->addPoint(Point(235,510));
+    pol->addPoint(Point(205,510));
+    pol->addPoint(Point(205,480));
+    pol->addPoint(Point(175,480));
+    pol->addPoint(Point(175,420));
+    pol->addPoint(Point(145,420));
+    pol->addPoint(Point(145,270));
+    pol->addPoint(Point(175,270));
+    pol->addPoint(Point(175,210));
+    pol->addPoint(Point(205,210));
+    pol->addPoint(Point(205,180));
+    pol->addPoint(Point(235,180));
+    pol->addPoint(Point(235,150));
+    pol->addPoint(Point(265,150));
+    pol->addPoint(Point(265,120));
+    pol->addPoint(Point(325,120));
+    pol->addPoint(Point(325,390));
+    pol->addPoint(Point(355,390));
+    pol->addPoint(Point(355,420));
+    pol->addPoint(Point(445,420));
+    pol->addPoint(Point(445,390));
+    pol->addPoint(Point(475,390));
+    pol->addPoint(Point(475,300));
+    pol->addPoint(Point(445,300));
+    pol->addPoint(Point(445,270));
+    pol->addPoint(Point(355,270));
+    pol->addPoint(Point(355,300));
+    pol->addPoint(Point(325,300));
+    return pol;
+}
+
 int main() {
   system("clear");
-  printpeluru()->print(fb);
+  printroda()->print(fb);
   return 0;
 }
