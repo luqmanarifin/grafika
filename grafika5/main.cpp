9726c862858f37dddec30852c272b77afeeb4a80
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-02-21 15:51:26 +0700

    Kentang goreng

--- grafika5/main.cpp
+++ grafika5/main.cpp
@@ -6,17 +6,21 @@
 #include "objects/belakang.h"
 #include "objects/window.h"
 #include "objects/Indonesia.h"
+#include "objects/kotak.h"
 
-
-const Point<int> window_corner_a = Point<int>(966, 518);
-const Point<int> window_corner_b = Point<int>(1400, 1000);
+const Point<double> window_corner_a = Point<double>(966, 544);
+const Point<double> window_corner_b = Point<double>(1366, 768);
 const int MAKS_ZOOM = 15;
 const int MIN_ZOOM = -10;
+
+#define ZOOM_IN 1.1
+#define ZOOM_OUT 0.9
 
 using namespace std;
 
 FrameBuffer fb;
 Window* window;
+Kotak* kotak = new Kotak(window_corner_a, window_corner_b);
 
 int main() {  
   system("clear");
@@ -24,13 +28,12 @@ int main() {
   window = new Window(window_corner_a.x, window_corner_a.y);
   Indonesia* indo = new Indonesia();
   indo->print(fb, new Belakang());
-  fb.print_exclude(window_corner_a, window_corner_b);
   
   Indonesia* small_indo = new Indonesia();
   small_indo->resize(0.3);
-  small_indo->move(675, 300);
+  small_indo->move(700, 325);
   small_indo->print(fb, window, 255, 0, 0, 0);
-  fb.print_include(window_corner_a, window_corner_b);
+  fb.print();
 
   int zoom = 0;
   while(true){ 
@@ -60,21 +63,20 @@ int main() {
 
     else if (cmd == 'q' || cmd == 'Q' ) {
       if(zoom < MAKS_ZOOM) {
-        indo->resize(1.1);
+        indo->resize(ZOOM_IN);
         zoom++;
       }
     }
     else if (cmd == 'e' || cmd == 'E' ) {
       if(zoom > MIN_ZOOM) { 
-        indo->resize(0.9);
+        indo->resize(ZOOM_OUT);
         zoom--;
       }
     }
     fb.clear();
     indo->print(fb, new Belakang());
-    fb.print_exclude(window_corner_a, window_corner_b);
     small_indo->print(fb, window, 255, 0, 0, 0);
-    fb.print_include(window_corner_a, window_corner_b);
+    fb.print();
   }
   return 0;
 }
---
+++ grafika5/objects/kotak.h
@@ -0,0 +1,16 @@
+#ifndef __KOTAK_H_
+#define __KOTAK_H_
+
+#include "../lib/polygon.h" 
+#include "../lib/point.h"
+
+struct Kotak : public Polygon {
+  Kotak(Point<double> a, Point<double> b) {
+    addPoint(a);
+    addPoint(Point<double>(b.x, a.y));
+    addPoint(b);
+    addPoint(Point<double>(a.x, b.y));
+  }
+};
+
+#endif
