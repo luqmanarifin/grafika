7f91a4af4bf42168827bdc25e55d8696fb1ee857
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-03-06 17:17:17 +0700

    Normalize and print polygon backup

--- lib/polygon_research.cpp
+++ lib/polygon_research.cpp
@@ -125,7 +125,7 @@ struct Polygon {
     }
     return sz;
   }
-  Polygon& print(FrameBuffer& fb) {
+  Polygon& normalize_and_print(FrameBuffer& fb) {
     Point<int>* points = new Point<int>[size];
     int size = normalize(points);
  
@@ -195,6 +195,75 @@ struct Polygon {
     }
     if ( a != NULL) delete[] a;
     //print_frame(fb,255,255,255,0);
+    return *this;
+  }
+  Polygon& print(FrameBuffer& fb) {
+    double ymin = 1e9, ymak = -1e9;
+    for(int i = 0; i < size; i++) {
+      ymin = min(ymin, points[i].y);
+      ymak = max(ymak, points[i].y);
+    }
+
+    double* a = new double[2 * size];
+    for(int y = ymin; y <= ymak; y++) {
+      int sz = 0;
+      for(int i = 0; i < size; i++) {
+        int j = (i + 1) % size;
+        double l = points[i].y;
+        double r = points[j].y;
+        if(min(l, r) <= y && y <= max(l, r)) {
+          double la = points[i].x;
+          double ra = points[j].x;
+          if(same(l, r)) {
+            a[sz++] = min(la, ra);
+            a[sz++] = max(la, ra);
+          } else {
+            double d = fabs(l - y)*fabs(la - ra)/fabs(l - r);
+            a[sz++] = la + (la < ra? d : -d);
+          }
+        }
+      }
+      sort(a, a + sz);
+      for(int i = 0; i + 1 < sz; i += 2) {
+        for(int j = (int) a[i]; j <= a[i + 1]; j++) {
+          fb.set(j, y, warna);
+        }
+      }
+    }
+    
+    double bmin = 1e9, bmak = -1e9;
+    for(int i = 0; i < size; i++) {
+      bmin = min(bmin, points[i].x);
+      bmak = max(bmak, points[i].x);
+    }
+
+    for(int b = bmin; b <= bmak; b++) {
+      int sz = 0;
+      for(int i = 0; i < size; i++) {
+        int j = (i + 1) % size;
+        double l = points[i].x;
+        double r = points[j].x;
+        if(min(l, r) <= b && b <= max(l, r)) {
+          double la = points[i].y;
+          double ra = points[j].y;
+          if(same(l, r)) {
+            a[sz++] = min(la, ra);
+            a[sz++] = max(la, ra);
+          } else {
+            double d = abs(l - b)*abs(la - ra)/abs(l - r);
+            a[sz++] = la + (la < ra? d : -d);
+          }
+        }
+      }
+      sort(a, a + sz);
+      for(int i = 0; i + 1 < sz; i += 2) {
+        for(int j = (int) a[i]; j <= a[i + 1]; j++) {
+          fb.set(b, j, warna);
+        }
+      }
+    }
+    if ( a != NULL) delete[] a;
+    print_frame(fb,255,255,255,0);
     return *this;
   }
   int MaxX(){
